d3c4cd65c8f5ae95cb3ec1b433850c1c2442ed97
Anatoliy Vikulov <vikulovtolya38@gmail.com>
2024-02-27 20:48:11 +0800

    up

--- src/js/index.js
+++ src/js/index.js
@@ -34,6 +34,68 @@ window.onload = () => {
   topSlider.style.height = `${sectionFirst.offsetHeight}px`;
 };
 
+// select
+$(".select").each(function () {
+  const _this = $(this),
+    selectOption = _this.find("option"),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(":selected"),
+    duration = 450; // длительность анимации
+
+  _this.hide();
+  _this.wrap('<div class="select"></div>');
+  $("<div>", {
+    class: "new-select",
+    text: _this.children("option:disabled").text(),
+  }).insertAfter(_this);
+
+  const selectHead = _this.next(".new-select");
+  $("<div>", {
+    class: "new-select__list",
+  }).insertAfter(selectHead);
+
+  const selectList = selectHead.next(".new-select__list");
+  for (let i = 1; i < selectOptionLength; i++) {
+    $("<div>", {
+      class: "new-select__item",
+      html: $("<span>", {
+        text: selectOption.eq(i).text(),
+      }),
+    })
+      .attr("data-value", selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+
+  const selectItem = selectList.find(".new-select__item");
+  selectList.slideUp(0);
+  selectHead.on("click", function () {
+    if (!$(this).hasClass("on")) {
+      $(this).addClass("on");
+      selectList.slideDown(duration);
+
+      selectItem.on("click", function () {
+        let chooseItem = $(this).data("value");
+
+        $("select").val(chooseItem).attr("selected", "selected");
+        selectHead.text($(this).find("span").text());
+
+        selectList.slideUp(duration);
+        selectHead.removeClass("on");
+      });
+    } else {
+      $(this).removeClass("on");
+      selectList.slideUp(duration);
+    }
+  });
+});
+
+// accordion
+$(document).ready(function () {
+  $(".panel-heading").click(function () {
+    $(this).toggleClass("in").next().slideToggle();
+  });
+});
+
 // sliders
 
 var swiper = new Swiper(".topSlider", {
@@ -78,6 +140,20 @@ var swiper4 = new Swiper(".video_slider", {
   spaceBetween: 25,
   pagination: {
     el: ".video_slider-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: { slidesPerView: 1, spaceBetween: 20 },
+    600: { slidesPerView: 2, spaceBetween: 20 },
+    1024: { slidesPerView: 3, spaceBetween: 25 },
+  },
+});
+
+var swiper5 = new Swiper(".news_slider", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  pagination: {
+    el: ".news_slider-pagination",
     clickable: true,
   },
   breakpoints: {
