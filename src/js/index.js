4a4889c2637154dea9f68f10de6f2542c249b173
Anatoliy Vikulov <vikulovtolya38@gmail.com>
2024-03-04 20:03:00 +0800

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const fixedHeader = document.querySelector(".header__fix");
 const headerBanner = document.querySelector(".header__banner");
 const headerTop = document.querySelector(".header__top");
-const sectionFirst = document.querySelector(".section-first__block");
+const sectionFirst = document.querySelector(".top__block");
 
 const topHeader = headerBanner.offsetHeight + headerTop.offsetHeight;
 
@@ -25,78 +25,6 @@ $(function () {
       $("#nav").removeClass("visible");
       $("#body").removeClass("no-scroll");
     });
-  });
-});
-
-const topSlider = document.querySelector(".first__block-slider");
-window.onload = () => {
-  console.log("document ready!");
-  topSlider.style.height = `${sectionFirst.offsetHeight}px`;
-};
-
-Fancybox.bind('[data-fancybox="gallery"]', {
-  // Your custom options for a specific gallery
-});
-
-// select
-$(".select").each(function () {
-  const _this = $(this),
-    selectOption = _this.find("option"),
-    selectOptionLength = selectOption.length,
-    selectedOption = selectOption.filter(":selected"),
-    duration = 450; // длительность анимации
-
-  _this.hide();
-  _this.wrap('<div class="select"></div>');
-  $("<div>", {
-    class: "new-select",
-    text: _this.children("option:disabled").text(),
-  }).insertAfter(_this);
-
-  const selectHead = _this.next(".new-select");
-  $("<div>", {
-    class: "new-select__list",
-  }).insertAfter(selectHead);
-
-  const selectList = selectHead.next(".new-select__list");
-  for (let i = 1; i < selectOptionLength; i++) {
-    $("<div>", {
-      class: "new-select__item",
-      html: $("<span>", {
-        text: selectOption.eq(i).text(),
-      }),
-    })
-      .attr("data-value", selectOption.eq(i).val())
-      .appendTo(selectList);
-  }
-
-  const selectItem = selectList.find(".new-select__item");
-  selectList.slideUp(0);
-  selectHead.on("click", function () {
-    if (!$(this).hasClass("on")) {
-      $(this).addClass("on");
-      selectList.slideDown(duration);
-
-      selectItem.on("click", function () {
-        let chooseItem = $(this).data("value");
-
-        $("select").val(chooseItem).attr("selected", "selected");
-        selectHead.text($(this).find("span").text());
-
-        selectList.slideUp(duration);
-        selectHead.removeClass("on");
-      });
-    } else {
-      $(this).removeClass("on");
-      selectList.slideUp(duration);
-    }
-  });
-});
-
-// accordion
-$(document).ready(function () {
-  $(".panel-heading").click(function () {
-    $(this).toggleClass("in").next().slideToggle();
   });
 });
 
@@ -166,3 +94,121 @@ var swiper5 = new Swiper(".news_slider", {
     1024: { slidesPerView: 3, spaceBetween: 25 },
   },
 });
+
+var swiper6 = new Swiper(".gallery__slider", {
+  slidesPerView: 5,
+  spaceBetween: 25,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: ".gallery-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: { slidesPerView: 2, centeredSlides: false, spaceBetween: 20 },
+    840: { slidesPerView: 3, centeredSlides: true, spaceBetween: 20 },
+    1024: { slidesPerView: 4, centeredSlides: true, spaceBetween: 25 },
+    1400: { slidesPerView: 5, centeredSlides: true, spaceBetween: 25 },
+  },
+});
+var swiper6 = new Swiper(".inst_slider", {
+  slidesPerView: 1,
+  spaceBetween: 25,
+  loop: true,
+  pagination: {
+    el: ".inst_slider-pagination",
+    clickable: true,
+  },
+});
+
+const topSlider = document.querySelector(".first__block-slider");
+if (topSlider != null) {
+  window.onload = () => {
+    console.log("document ready!");
+    topSlider.style.height = `${sectionFirst.offsetHeight}px`;
+  };
+}
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  // Your custom options for a specific gallery
+});
+
+// select
+$(".select").each(function () {
+  const _this = $(this),
+    selectOption = _this.find("option"),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(":selected"),
+    duration = 450; // длительность анимации
+
+  _this.hide();
+  _this.wrap('<div class="select"></div>');
+  $("<div>", {
+    class: "new-select",
+    text: _this.children("option:disabled").text(),
+  }).insertAfter(_this);
+
+  const selectHead = _this.next(".new-select");
+  $("<div>", {
+    class: "new-select__list",
+  }).insertAfter(selectHead);
+
+  const selectList = selectHead.next(".new-select__list");
+  for (let i = 1; i < selectOptionLength; i++) {
+    $("<div>", {
+      class: "new-select__item",
+      html: $("<span>", {
+        text: selectOption.eq(i).text(),
+      }),
+    })
+      .attr("data-value", selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+
+  const selectItem = selectList.find(".new-select__item");
+  selectList.slideUp(0);
+  selectHead.on("click", function () {
+    if (!$(this).hasClass("on")) {
+      $(this).addClass("on");
+      selectList.slideDown(duration);
+
+      selectItem.on("click", function () {
+        let chooseItem = $(this).data("value");
+
+        $("select").val(chooseItem).attr("selected", "selected");
+        selectHead.text($(this).find("span").text());
+
+        selectList.slideUp(duration);
+        selectHead.removeClass("on");
+      });
+    } else {
+      $(this).removeClass("on");
+      selectList.slideUp(duration);
+    }
+  });
+});
+
+// accordion
+$(document).ready(function () {
+  $(".panel-heading").click(function () {
+    $(this).toggleClass("in").next().slideToggle();
+  });
+});
+
+// filters
+const filterBox = document.querySelectorAll(".box");
+const clearClass = () => {
+  document.querySelectorAll(".filters__button > li").forEach((elem) => {
+    elem.classList.remove("active");
+  });
+};
+
+if (document.querySelector(".filters__button") != null) {
+  document
+    .querySelector(".filters__button")
+    .addEventListener("click", (event) => {
+      if (event.target.tagName !== "LI") return false;
+      clearClass();
+      event.target.classList.add("active");
+    });
+}
